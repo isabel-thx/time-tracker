41d70b1fb17e03e60e59bf527bb794f19f9861c3
isabel-thx <isabelhx.teh@gmail.com>
2018-07-04 22:31:08 +0800

    User can sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user = User.authenticate(params[:session])
+      session[:user_id] = @user.id
+      redirect_to projects_path
+    else
+      respond_to do |format|
+        format.html { redirect_to sign_in_path, notice: 'Invalid email/password combination' }
+      end
+    end
+  end
+
+  def destroy
+    @current_user = session[:user_id] = nil
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+
+  resources :users, except: :index
+
+  resources :projects do
+  	resources :tasks
+  end
+
+  get "/sign_in" => 'sessions#new', as: "sign_in"
+  post "/sign_in" => 'sessions#create'
+  delete "/sign_out" => 'sessions#destroy', as: "sign_out"
 end
